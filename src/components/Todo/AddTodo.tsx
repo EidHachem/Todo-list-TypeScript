b390df2c0fe5bba2e43844af3a730623ee0e8255
EidHachem <eidhachem1@gmail.com>
2022-11-09 18:39:04 +0200

    Add design to form and fix some issues with time and priority fields

--- src/components/Todo/AddTodo.tsx
+++ src/components/Todo/AddTodo.tsx
@@ -3,6 +3,8 @@ import React, { useRef } from 'react';
 const AddTodo: React.FC<{
   addTodoHandler: (text: string, priority: string, time: string) => void;
 }> = (props) => {
+  const today = new Date();
+  const time = today.getHours() + ':' + today.getMinutes();
   const textRef = useRef<HTMLInputElement>(null);
   const priorityRef = useRef<HTMLSelectElement>(null);
   const timeRef = useRef<HTMLInputElement>(null);
@@ -22,28 +24,28 @@ const AddTodo: React.FC<{
     }
 
     textRef.current!.value = '';
-    priorityRef.current!.value = '';
-    timeRef.current!.value = '';
+    priorityRef.current!.value = '0';
+    timeRef.current!.value = time;
   };
 
   return (
-    <div>
-      <div>
-        <h1>Add a todo</h1>
-        <form onSubmit={submitHandler}>
-          <input type="text" placeholder="Buy groceries..." ref={textRef} />
-          <input type="time" ref={timeRef} />
-          <select defaultValue={0} ref={priorityRef}>
-            <option value="0" disabled>
-              Priority
-            </option>
-            <option value="low">Low</option>
-            <option value="medium">Medium</option>
-            <option value="high">High</option>
-          </select>
-          <button type="submit">Add Todo</button>
-        </form>
-      </div>
+    <div className="mx-auto">
+      <h1 className="text-center py-4 text-4xl text-orange-600">Add a todo</h1>
+      <form className="flex justify-center items-center gap-3" onSubmit={submitHandler}>
+        <input type="text" placeholder="Buy groceries..." ref={textRef} />
+        <input type="time" placeholder={time} defaultValue={time} ref={timeRef} />
+        <select defaultValue={0} ref={priorityRef}>
+          <option value="0" disabled>
+            Priority
+          </option>
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+        <button type="submit" className="bg-orange-600 text-white rounded py-2 px-3">
+          Add Todo
+        </button>
+      </form>
     </div>
   );
 };
