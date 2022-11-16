bf28033a5fdb808c17566d3daf8ef77becd23af7
EidHachem <eidhachem1@gmail.com>
2022-11-16 22:05:23 +0200

    Add notes to the routes and change bg color

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import Navbar from './components/Navbar/Navbar';
 import Todos from './components/Todo/Todos';
 import Expenses from './components/Budget/Expenses';
 import Home from './components/Home/Home';
+import Notes from './components/Notes/Notes';
 
 const App: FC = () => {
   return (
-    <div className="bg-green-800 flex flex-col min-h-[100vh]">
+    <div className="bg-gradient-to-tr to-green-800 from-green-200 flex flex-col min-h-[100vh]">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todos" element={<Todos />} />
         <Route path="/expenses" element={<Expenses />} />
+        <Route path="/notes" element={<Notes />} />
         <Route path="*" element={<div>Not found</div>} />
       </Routes>
     </div>
