53620ef0a942bb5f1910bd8513830effac5580b9
EidHachem <eidhachem1@gmail.com>
2022-11-18 17:53:30 +0200

    Add notes to home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { FaList, FaMoneyBill } from 'react-icons/fa';
+import { FaList, FaMoneyBill, FaPen } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1 className="text-center text-3xl text-orange-600 py-12">Welcome</h1>
-      <div className="mx-auto max-w-[20%] grid grid-cols-2 gap-5">
+      <div className="mx-auto max-w-[25%] grid grid-cols-2 gap-5">
         <Link to="/todos" className="hover:scale-110">
           <div className="border-2 border-white flex flex-col items-center justify-center gap-4 py-10">
             <FaList className="text-6xl text-orange-600 mx-auto block" />
@@ -17,6 +17,12 @@ const Home = () => {
           <div className="border-2 border-white flex flex-col items-center justify-center gap-4 py-10">
             <FaMoneyBill className="text-6xl text-orange-600 mx-auto block" />
             <p className="text-center text-2xl text-white">Expenses</p>
+          </div>
+        </Link>
+        <Link to="/notes" className="hover:scale-110">
+          <div className="border-2 border-white flex flex-col items-center justify-center gap-4 py-10">
+            <FaPen className="text-6xl text-orange-600 mx-auto block" />
+            <p className="text-center text-2xl text-white">Notes</p>
           </div>
         </Link>
       </div>
